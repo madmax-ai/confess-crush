931e774bd9cfb9ff6538684331405579a57ebc81
madmax-ai <78438948+madmax-ai@users.noreply.github.com>
2024-02-16 19:27:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Web Title",
+    introTitle: 'ena',
+    introDesc: `kenapa hari ii merajuk :(`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    title: 'ena jangan lah merajuk:(',
+    desc: 'ena merajuk lagi ke? ',
+    btnYes: 'tak :)',
+    btnNo: 'ye',
+    question: 'kenapa ena merajuk? :/',
+    btnReply: 'Send to you <3',
+    reply: '',
+    mess: 'heheheheh',
+    messDesc: 'jangan merajuk lama ii..hehhehe',
+    btnAccept: 'heh',
+    messLink: 'https://github.com/zukahai/Confess-Crush' // Your friend's messaging link. Example: https://www.facebook.com/messages/t/100014188333536
 }
